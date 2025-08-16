ce19bc5226c5271dcfa59bfe1d1f1607201b04f9
EthanJLiu <114434593+EthanJLiu@users.noreply.github.com>
2025-08-15 17:40:02 -0700

    day 4 part2

--- Day4/Advent2024_Day4.py
+++ Day4/Advent2024_Day4.py
@@ -33,10 +33,33 @@ def p1():
                     num_matches += search_routes("X", r, c, direction)
 
     print(num_matches)
+# p1()    
 
-        
+def p2():
+    lines = []
+    with open("Day4/Day4_Input.txt") as f:
+        for line in f:
+            split_line = [c for c in line]
+            lines.append(split_line)
+            print(split_line)
+   
+    def search_corners(row, col):
+        if row == 0 or col == 0 or row == len(lines)-1 or col == len(lines[0])-1:
+            return 0
+        top_left_to_bottom_right = (lines[row-1][col-1] == "M" and lines[row+1][col+1] == "S") or (lines[row-1][col-1] == "S" and lines[row+1][col+1] == "M")
+        bottom_left_to_top_right = (lines[row+1][col-1] == "M" and lines[row-1][col+1] == "S") or (lines[row+1][col-1] == "S" and lines[row-1][col+1] == "M")
 
+        if top_left_to_bottom_right and bottom_left_to_top_right:
+            return 1
+        return 0
 
-p1()    
+    num_matches = 0
 
-#140 x 140
+    for r in range(len(lines)):
+        for c in range(len(lines[r])):
+            if lines[r][c] == "A":
+                num_matches += search_corners(r, c)
+    print(num_matches)
+
+p2()    
+
